ff60dac82fe82eeaefacedcd13bc0604a322343e
Tristan Kitto <kittr801@student.otago.ac.nz>
2023-07-17 16:31:07 +1200

    Added colours for all conservation statuses

--- script.js
+++ script.js
@@ -27,9 +27,43 @@ function fetchData() {
                     imageElement.src = bird.photo.source;
                     imageElement.alt = bird.primary_name;
 
+                    const status = bird.status;
+                    switch (status) {
+                        case 'Not Threatened':
+                            statusDot.style.backgroundColor = '#02a028';
+                            break;
+                        case 'Naturally Uncommon':
+                            statusDot.style.backgroundColor = '#649a31';
+                            break;
+                        case 'Relict':
+                            statusDot.style.backgroundColor = '#99cb68';
+                            break;
+                        case 'Recovering':
+                            statusDot.style.backgroundColor = '#fecc33';
+                            break;
+                        case 'Declining':
+                            statusDot.style.backgroundColor = '#fe9a01';
+                            break;
+                        case 'Nationally Increasing':
+                            statusDot.style.backgroundColor = '#c26967';
+                            break;
+                        case 'Nationally Vulnerable':
+                            statusDot.style.backgroundColor = '#9b0000';
+                            break;
+                        case 'Nationally Endangered':
+                            statusDot.style.backgroundColor = '#660032';
+                            break;
+                        case 'Nationally Critical':
+                            statusDot.style.backgroundColor = '#320033';
+                            break;
+                        default:
+                            statusDot.style.backgroundColor = 'black';
+                            break;
+                    }
+
                     primaryNameElement.textContent = bird.primary_name;
                     englishNameElement.textContent = bird.english_name;
-                    statusElement.textContent = bird.status;
+                    statusElement.textContent = status;
                     photoCreditElement.textContent = bird.photo.credit;
 
                     overlayElement.appendChild(primaryNameElement);
